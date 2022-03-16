1d212b84b38a92cd847a78682caeeb131c9d081b
skose.vbt <serhat.kose@vbt.com.tr>
2022-03-16 23:58:35 +0300

    controller test class add

---
+++ src/test/java/com/serhat/licensingservice/controllers/LicenseServiceControllerTest.java
@@ -0,0 +1,24 @@
+package com.serhat.licensingservice.controllers;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LicenseServiceControllerTest {
+
+    @Test
+    void getLicenses() {
+    }
+
+    @Test
+    void updateLicenses() {
+    }
+
+    @Test
+    void saveLicenses() {
+    }
+
+    @Test
+    void deleteLicenses() {
+    }
+}
